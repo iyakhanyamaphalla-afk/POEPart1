88bf9aedb5df116dc7fb440d4da0fd97a09ef875
Student <Student@LabVM2049939.gjujar4nu0fungmpo0oln4sfpa.jnbx.internal.cloudapp.net>
2026-09-17 14:35:56 +0200

    Login.page Testing Username correctly formatted testing username is not correctly formatted

--- src/test/java/com/mycompany/poepart1/loginIT.java
+++ src/test/java/com/mycompany/poepart1/loginIT.java
@@ -16,14 +16,36 @@ public class loginIT {
 @Test
     public void testUsernameCorrectlyFormatted() {
         
-        // Setup: Create a user with a valid username
+        //  Create a user with a valid username
         login user = new login("Kyle", "Smith", "kyl_1", "Ch&&sec@ke99!", "+27838968976");
         
         // Action & Assertion: Check if the method returns true
         assertTrue(user.CheckUserName(), "CheckUserName() should return TRUE for 'kyl_1'");
     }
-        
+       @Test
+    public void testUsernameIncorrectlyFormatted_AssertFalse() {
+        // 1. Setup the test data (Invalid username: no underscore, too long)
+        login user = new login("Kyle", "Smith", "kyle!!!!!!!", "Ch&&sec@ke99!", "+27838968976");
+
+        // 2. Call the method that validates the username
+        // This method returns 'true' if valid, and 'false' if invalid
+        boolean isUsernameValid = user.CheckUserName();
+
+        // 3. Assert that the result is FALSE (because the username is invalid)
+        assertFalse(isUsernameValid, "CheckUserName() should return FALSE for 'kyle!!!!!!!' because it has no underscore and is too long.");
     }
+    
+    
+    
+   
+}
+    
+    
+    
+    
+    
+    
+    
 
 
 
